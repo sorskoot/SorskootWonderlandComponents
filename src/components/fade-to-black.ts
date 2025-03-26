a483eddb990c5030c9f670a1e039b0853ba1140b
Timmy Kokke <mental_c@hotmail.com>
2025-03-26 21:18:29 +0100

    Enhance FadeToBlack and SelfDestruct components with improved documentation and validation; streamline imports and error handling

--- src/components/fade-to-black.ts
+++ src/components/fade-to-black.ts
@@ -1,13 +1,20 @@
-import {Component, Emitter, Material, MeshComponent, Object3D} from '@wonderlandengine/api';
+import {Component, MeshComponent, Object3D} from '@wonderlandengine/api';
 import {property} from '@wonderlandengine/api/decorators.js';
 import {FlatOpaque} from '../types/materials.js';
 import {wlUtils} from '../utils/wlUtils.js';
 
+/**
+ * Enum representing the current transition state
+ */
 enum TransitionType {
+    /** Fading to black (increasing alpha) */
     FadeToBlack,
+    /** Fading from black (decreasing alpha) */
     FadeFromBlack,
+    /** No transition in progress */
     None,
 }
+
 /**
  * FadeToBlack component
  * 
@@ -38,65 +45,105 @@ private async transition(
 ```
  */
 export class FadeToBlack extends Component {
+    /** Component registration name in Wonderland Engine */
     static TypeName = 'fade-to-black';
 
+    /**
+     * The object that will be faded
+     */
     @property.object({required: true})
     fadeObject!: Object3D;
 
+    /**
+     * Time in seconds for the fade transition
+     */
     @property.float(0.5)
     fadeTransitionTime = 0.5;
 
+    /**
+     * Color to fade to/from [r, g, b, a]
+     */
     @property.color(0.0, 0.0, 0.0, 1.0)
     fadeColor = [0.0, 0.0, 0.0, 1.0];
 
+    /** Current transition state */
     private _transitionInProgress = TransitionType.None;
+
+    /** Material used for fading */
     private _fadeMaterial!: FlatOpaque;
+
+    /** Current alpha value */
     private _alphaValue = 1.0;
 
+    /** Promise resolver function */
+    private _resolve: (value: void) => void = () => {};
+
+    /**
+     * Initialize the component
+     */
     init(): void {
         // Disable the object on init
         wlUtils.setActive(this.object, false);
         this.object.resetPosition();
     }
 
-    start() {
+    /**
+     * Validate required properties and setup component
+     */
+    start(): void {
+        if (!this.fadeObject) {
+            throw new Error('fade-to-black: fadeObject is required');
+        }
+
+        if (this.fadeTransitionTime <= 0) {
+            throw new Error('fade-to-black: fadeTransitionTime must be greater than 0');
+        }
+
         const meshComponent = this.fadeObject.getComponent(MeshComponent);
         if (!meshComponent) {
-            console.error(
-                'FadeToBlack component requires a mesh component on the fadeObject'
-            );
-            return;
+            throw new Error('fade-to-black: fadeObject must have a MeshComponent');
         }
+
         this._fadeMaterial = meshComponent.material as FlatOpaque;
         if (!this._fadeMaterial) {
-            console.error(
-                'FadeToBlack component requires a FlatOpaque material on the fadeObject'
-            );
-            return;
+            throw new Error('fade-to-black: fadeObject must have a FlatOpaque material');
         }
     }
 
-    private _resolve: (value: void | PromiseLike<void>) => void = () => {};
-
+    /**
+     * Begin fade to black transition
+     * @returns Promise that resolves when fade is complete
+     */
     fadeToBlack(): Promise<void> {
-        this._transitionInProgress = TransitionType.FadeToBlack;
-        this._setObjectsActive();
-        this._alphaValue = 0;
-        this._fadeMaterial.setColor(this.fadeColor);
-        const completionPromise = new Promise<void>((r) => (this._resolve = r));
-        return completionPromise;
+        return this._startTransition(TransitionType.FadeToBlack, 0);
     }
 
+    /**
+     * Begin fade from black transition
+     * @returns Promise that resolves when fade is complete
+     */
     fadeFromBlack(): Promise<void> {
-        this._transitionInProgress = TransitionType.FadeFromBlack;
-        this._setObjectsActive();
-        this._alphaValue = 1;
-        this._fadeMaterial.setColor(this.fadeColor);
-        const completionPromise = new Promise<void>((r) => (this._resolve = r));
-        return completionPromise;
+        return this._startTransition(TransitionType.FadeFromBlack, 1);
     }
 
-    private _setObjectsActive() {
+    /**
+     * Start a transition with the specified type and initial alpha
+     * @param type Type of transition to start
+     * @param initialAlpha Initial alpha value
+     * @returns Promise that resolves when transition is complete
+     */
+    private _startTransition(type: TransitionType, initialAlpha: number): Promise<void> {
+        this._transitionInProgress = type;
+        this._setObjectsActive();
+        this._alphaValue = initialAlpha;
+        this._fadeMaterial.setColor(this.fadeColor);
+        return new Promise<void>((resolve) => (this._resolve = resolve));
+    }
+
+    /**
+     * Set objects active based on current transition state
+     */
+    private _setObjectsActive(): void {
         wlUtils.setActive(this.object, true);
         if (
             this._transitionInProgress === TransitionType.FadeToBlack ||
@@ -106,11 +153,16 @@ export class FadeToBlack extends Component {
         }
     }
 
-    update(dt: number) {
+    /**
+     * Update the component each frame
+     * @param dt Delta time since last frame
+     */
+    update(dt: number): void {
         if (this._transitionInProgress === TransitionType.None) {
             return;
         }
 
+        // Handle different transition types
         if (this._transitionInProgress === TransitionType.FadeToBlack) {
             this._alphaValue += dt / this.fadeTransitionTime;
             if (this._alphaValue >= 1) {
@@ -128,16 +180,19 @@ export class FadeToBlack extends Component {
             }
         }
 
-        if (
-            this._transitionInProgress === TransitionType.FadeFromBlack ||
-            this._transitionInProgress === TransitionType.FadeToBlack
-        ) {
-            this._fadeMaterial.setColor([
-                this.fadeColor[0],
-                this.fadeColor[1],
-                this.fadeColor[2],
-                this._alphaValue,
-            ]);
-        }
+        // Update material color with current alpha value
+        this._updateMaterialColor();
+    }
+
+    /**
+     * Updates the material color with the current alpha value
+     */
+    private _updateMaterialColor(): void {
+        this._fadeMaterial.setColor([
+            this.fadeColor[0],
+            this.fadeColor[1],
+            this.fadeColor[2],
+            this._alphaValue,
+        ]);
     }
 }
--- src/components/self-destruct.ts
+++ src/components/self-destruct.ts
@@ -1,15 +1,34 @@
-import {Component, Object3D} from '@wonderlandengine/api';
+import {Component} from '@wonderlandengine/api';
 import {property} from '@wonderlandengine/api/decorators.js';
 
+/**
+ * Component that destroys its object after a specified time
+ */
 export class SelfDestruct extends Component {
     static TypeName = 'self-destruct';
 
+    /**
+     * Time in seconds after which the object will be destroyed
+     */
     @property.float(1.0)
     lifeTime = 1.0;
 
+    /** Tracks elapsed time since component activation */
     private _time: number = 0;
-    start(): void {}
 
+    /**
+     * Validates component properties on start
+     */
+    start(): void {
+        if (this.lifeTime <= 0) {
+            throw new Error('self-destruct: lifeTime must be greater than 0');
+        }
+    }
+
+    /**
+     * Updates elapsed time and destroys object when lifetime is reached
+     * @param dt Delta time in seconds
+     */
     update(dt: number): void {
         this._time += dt;
         if (!this.object.isDestroyed && this._time > this.lifeTime) {
